d8e17bf2bd17b7a633a1f2077ddc84251125385f
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-14 18:17:50 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace oop8_orders
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(954, 232);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 270);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "Список замовлень";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop8_orders
+{
+    public partial class MainForm : Form
+    {
+        // Клас для представлення замовлення
+        public class Order
+        {
+            public int OrderNumber { get; set; }
+            public string CustomerName { get; set; }
+            public string ContactPhone { get; set; }
+            public string City { get; set; }
+            public string PostalCode { get; set; }
+            public string OrderStatus { get; set; }
+        }
+
+        // Список для зберігання замовлень
+        private List<Order> orders;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Ініціалізація списку та додавання вигаданих замовлень
+            orders = new List<Order>
+            {
+                new Order { OrderNumber = 1, CustomerName = "Клішина Ольга Володимирівна", ContactPhone = "+0676755483", City = "Київ", PostalCode = "23", OrderStatus = "В обробці" },
+                new Order { OrderNumber = 2, CustomerName = "Цимбал Олена Олексіївна", ContactPhone = "+0936578478", City = "Львів", PostalCode = "78", OrderStatus = "Відправлено" },
+                new Order { OrderNumber = 3, CustomerName = "Гибало Андрій Станіславович", ContactPhone = "+0967856454", City = "Одеса", PostalCode = "59", OrderStatus = "Доставлено" },
+                new Order { OrderNumber = 4, CustomerName = "Доценко Максим Іванович", ContactPhone = "+0663655478", City = "Харків", PostalCode = "12", OrderStatus = "В обробці" }
+            };
+
+            // Заповнення таблиці даними при завантаженні форми
+            PopulateTable();
+        }
+
+        private void PopulateTable()
+        {
+            // Очищення таблиці перед заповненням
+            dataGridView1.Rows.Clear();
+
+            dataGridView1.Columns.Add("Header", "Номер замовлення");
+            dataGridView1.Columns.Add("Header", "ПІБ клієнта");
+            dataGridView1.Columns.Add("Header", "Номер телефону");
+            dataGridView1.Columns.Add("Header", "Місто");
+            dataGridView1.Columns.Add("Header", "Номер пошти");
+            dataGridView1.Columns.Add("Header", "Статус");
+
+            // Додавання замовлень до таблиці
+            foreach (var order in orders)
+            {
+                dataGridView1.Rows.Add(
+                    order.OrderNumber,
+                    order.CustomerName,
+                    order.ContactPhone,
+                    order.City,
+                    order.PostalCode,
+                    order.OrderStatus
+                );
+            }
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
